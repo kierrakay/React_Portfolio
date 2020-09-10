9068f6f4c060f6e31dcc4a06fceee30dce82e666
kierra <51520037+kierrakay@users.noreply.github.com>
2020-09-10 11:11:18 -0400

    successfuly passing props from blog component to comments container.

--- frontend/src/containers/CommentsContainer.js
+++ frontend/src/containers/CommentsContainer.js
@@ -3,11 +3,16 @@ import CommentInput from '../components/comments/CommentInput'
 import Comments from '../components/comments/Comments'
 
 class CommentsContainer extends React.Component {
+    myFunc() {
+        return this.props.blog && this.props.blog.comments
+    }
     render () {
         return (
             <div>
                 <CommentInput />
-                <Comments/>
+                {/* have to do && (issa shorthand teranary) because upon refresh on the show page the redux store clears out and we lose our props. first 
+                refresh is always undefined its the 2nd render that grabs the props. so basically if this.props.blog exxists then go to this.props.blog.comments */}
+                <Comments comments={this.myFunc()} />
             </div>
         )
     }
